b03505853e81f2333252947f6b682defd5fd6d54
Илья Тимофеев <Mcdos14@ya.ru>
2022-08-13 05:54:28 +0500

    s

--- Case4/Program.cs
+++ Case4/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Case4
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Dictionary<string, int> values = new();
+
+            do
+            {
+                
+                string s = Console.ReadLine();
+                var regex1 = new Regex(@"^[a-z]{0,9}=[a-z]{0,9}$");
+                var regex2 = new Regex(@"^[a-z]{0,9}=[0-10000]{0,9}$");
+                var regex3 = new Regex(@"[0-10000]");
+                if (regex1.IsMatch(s) ^ regex2.IsMatch(s))
+                {
+                    Console.WriteLine(s);
+                    string[] text = s.Split('=');
+                    if (regex3.IsMatch(text[1])) 
+                    {
+                        int i = int.Parse(text[1].ToString());
+                        if (values.ContainsKey(text[0]))
+                        {
+                            values.Remove(text[0]);
+                            values.Add(text[0], i);
+                        }
+                        else values.Add(text[0], i);
+                    }
+                    else
+                    {
+                        if (values.ContainsKey(text[0]))
+                        {
+                            values.TryGetValue(text[0], out int k);
+                            values.Remove(text[0]);
+                            values.Add(text[0], k);
+                        }
+                        else values.Add(text[0], 0);
+                    }
+                }
+
+
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            foreach (var person in values)
+            {
+                Console.WriteLine($"key: {person.Key}  value: {person.Value}");
+            }
         }
+        
     }
 }
